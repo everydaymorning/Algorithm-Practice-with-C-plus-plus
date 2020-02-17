eb1a5f2182cc0b2c456104683d0043c2454cec4a
everydaymorning <zmflrqll@naver.com>
2020-02-17 19:58:38 +0900

    20200217 simulation

---
+++ TestProject/TestProject/14890.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, l;
+int MAP[101][101];
+bool check[101][101];
+
+int main() {
+	cin >> n >> l;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> MAP[i][j];
+		}
+	}
+
+	for (int i = 0; i < n - l; i++) {
+		for (int j = 0; j < n - l; j++) {
+			
+		}
+	}
+
+}
