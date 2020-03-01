84b212d60a10d4363cfd9756c29e083f99f2f13e
everydaymorning <zmflrqll@naver.com>
2020-03-01 23:30:45 +0900

    20200301 samsung sw

--- TestProject/TestProject/14501_2.cpp
+++ TestProject/TestProject/14501_2.cpp
@@ -2,38 +2,38 @@
 using namespace std;
 int n;
 vector<pair<int, int>> v;
+int mx;
+void dfs(int idx, int sum, int sum1) {
+	if (idx == n) {
+		cout << "idx == n" << '\n';
+		cout << "sum: " << sum << '\n';
+		mx = max(mx, sum);
+		return;
+	}
+	else if (idx > n) {
+		cout << "idx > n" << '\n';
+		cout << "idx: " << idx << " sum - sum1: " << sum - sum1 << '\n';
+		mx = max(mx, sum - sum1);
+		return;
+	}
+	for (int i = idx; i < v.size(); i++) {
+		int t = v[i].first;
+		int p = v[i].second;
+		cout << "i: " << i << " t: " << t << '\n';
+		dfs(i + t, sum + p, p);
+	}
+}
 int main() {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
 	cin >> n;
-	int mx = 0;
 	for (int i = 0; i < n; i++) {
 		int a, b;
 		cin >> a >> b;
 		v.push_back({ a,b });
 	}
 
-	for (int i = 0; i < n; i++) {
-		int sum = 0;
-		int t = v[i].first;
-		int p = v[i].second;
-		if (t + i > n) continue;
-		sum += p;
-		for (int j = i + t - 1; j < n; j++) {
-			int t1 = v[j].first;
-			int p1 = v[j].second;
-			if (t1 + j > n) continue;
-			sum += p1;
-			for (int k = j + 1; k < n; k++) {
-				int t2 = v[k].first;
-				int p2 = v[k].second;
-				if (t2 + k > n) continue;
-				sum += p2;
-			}
-		}
-		mx = max(sum, mx);
-
-	}
+	dfs(0, 0, 0);
 	cout << mx;
 	return 0;
 }
