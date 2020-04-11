6b7cf6565cc7db33fd66c5320e55f196cb207eb6
everydaymorning <zmflrqll@naver.com>
2020-04-11 14:41:42 +0900

    20200411 네이버 핵데이 코테

--- TestProject/TestProject/naver1.cpp
+++ TestProject/TestProject/naver1.cpp
@@ -1,6 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int check[1001];
 int main() {
+	vector<vector<int>> delivery;
+	int n;
+	cin >> n;
+	delivery.push_back({ 3,5,0 });
+	delivery.push_back({ 1,3,1 });
+	delivery.push_back({ 5,4,0 });
+	delivery.push_back({ 2,5,0 });
 
+	string answer;
+	for (int k = 0; k < 2; k++) {
+		for (int i = 0; i < delivery.size(); i++) {
+			if (delivery[i][2] == 1) {
+				check[delivery[i][0]] = 2;
+				check[delivery[i][1]] = 2;
+			}
+			else if (delivery[i][2] == 0) {
+				if (check[delivery[i][0]] == 2 || check[delivery[i][1]] == 2) {
+					if (check[delivery[i][0]] == 2) {
+						check[delivery[i][1]] = 3;
+					}
+					else if (check[delivery[i][1]] == 2) {
+						check[delivery[i][0]] = 3;
+					}
+				}
+				else if (check[delivery[i][0]] == 3 || check[delivery[i][1]] == 3) {
+					if (check[delivery[i][0]] == 3) {
+						check[delivery[i][1]] = 0;
+					}
+					else if (check[delivery[i][1]] == 3) {
+						check[delivery[i][0]] = 0;
+					}
+				}
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		if (check[i] == 2) {
+			answer += "O";
+		}
+		else if (check[i] == 3) {
+			answer += "X";
+		}
+		else {
+			answer += "?";
+		}
+	}
+	cout << answer;
+	return 0;
 }
--- TestProject/TestProject/naver2.cpp
+++ TestProject/TestProject/naver2.cpp
@@ -2,5 +2,48 @@
 using namespace std;
 
 int main() {
+	vector<string> id_list;
+	id_list.push_back("JAY");
+	id_list.push_back("JAY ELLE JAY MAY");
+	id_list.push_back("MAY ELLE MAY");
+	id_list.push_back("ELLE MAY");
+	id_list.push_back("ELLE ELLE ELLE");
+	id_list.push_back("MAY");
 
+
+	int k;
+	cin >> k;
+
+	map<string, int> MAP;
+	int ans = 0;
+	for (int i = 0; i < id_list.size(); i++) {
+		vector<string> v;
+		string s;
+
+		map<string, int> M1;
+		for (int j = 0; j < id_list[i].size(); j++) {
+			
+			if (id_list[i][j] == ' ') {
+				v.push_back(s);
+				s = "";
+			}
+			else s += id_list[i][j];
+		}
+		v.push_back(s);
+		
+		for (int j = 0; j < v.size(); j++) {
+			M1[v[j]]++;
+			if (M1[v[j]] > 1) continue;
+			if (MAP[v[j]] < k) {
+				cout << "MAP: " << v[j] << "  " << MAP[v[j]] << '\n';
+				
+				ans++;
+			}
+			MAP[v[j]]++;
+
+		}
+
+	}
+	cout << ans;
+	return 0;
 }
