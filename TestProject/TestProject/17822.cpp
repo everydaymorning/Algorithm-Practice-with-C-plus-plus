85755d5c94c8dcc6874b7330193c962be4c77563
everydaymorning <zmflrqll@naver.com>
2020-04-15 17:20:17 +0900

    20200415 삼성 시뮬레이션

---
+++ TestProject/TestProject/17822.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, t;
+int MAP[51][51];
+int main() {
+	cin >> n >> m >> t;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> MAP[i][j];
+		}
+	}
+
+	return 0;
+}
