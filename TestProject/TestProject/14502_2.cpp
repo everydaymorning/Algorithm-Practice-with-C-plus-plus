4ab9a2083104de13a9f13e80f0c34e43284b3b17
everydaymorning <zmflrqll@naver.com>
2020-04-28 14:25:51 +0900

    20200428 삼성 기출 스터디

---
+++ TestProject/TestProject/14502_2.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+int MAP[8][8];
+int TEMP[8][8];
+bool check[8][8];
+int n, m;
+
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+vector<pair<int, int>> v;
+queue<pair<int, int>> virus;
+int MAX;
+void find_max() {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (TEMP[i][j] == 0) sum++;
+		}
+	}
+	MAX = max(MAX, sum);
+}
+void setting() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			TEMP[i][j] = MAP[i][j];
+		}
+	}
+}
+void bfs() {
+	for (int i = 0; i < v.size(); i++) {
+		int x = v[i].first;
+		int y = v[i].second;
+		virus.push({ x,y });
+		check[x][y] = true;
+	}
+
+	while (!virus.empty()) {
+		int x = virus.front().first;
+		int y = virus.front().second;
+		virus.pop();
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+			if (TEMP[nx][ny] == 2 || TEMP[nx][ny] == 1) continue;
+			if (check[nx][ny]) continue;
+			virus.push({ nx,ny });
+			check[nx][ny] = true;
+			TEMP[nx][ny] = 2;
+		}
+	}
+
+	
+}
+
+void dfs(int cnt) {
+	if (cnt == 3) {
+		setting();
+		bfs();
+		memset(check, false, sizeof(check));
+		find_max();
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (MAP[i][j] == 0) {
+				MAP[i][j] = 1;
+				dfs(cnt + 1);
+				MAP[i][j] = 0;
+			}
+		}
+	}
+}
+
+void INPUT() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> MAP[i][j];
+			if (MAP[i][j] == 2) v.push_back({ i,j });
+		}
+	}
+}
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	INPUT();
+	dfs(0);
+	cout << MAX;
+	return 0;
+}
---
+++ TestProject/TestProject/14503_3.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+int MAP[51][51];
+bool check[51][51];
+int n, m, x, y, d, cnt;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+void INPUT() {
+	cin >> n >> m;
+	cin >> x >> y >> d;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> MAP[i][j];
+		}
+	}
+}
+void move() {
+	while (1) {
+		if (!check[x][y]) {
+			check[x][y] = true;
+			cnt++;
+		}
+		bool flag = false;
+
+		for (int dir = 0; dir < 4; dir++) {
+			d = (d + 3) % 4;
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			if (MAP[nx][ny] == 1 || check[nx][ny]) continue;
+			x = nx;
+			y = ny;
+			flag = true;
+			break;
+		}
+		if (!flag) {
+			x = x + dx[(d + 2) % 4];
+			y = y + dy[(d + 2) % 4];
+			if (MAP[x][y] == 1) break;
+
+		}
+	}
+	cout << cnt;
+}
+void solve() {
+	INPUT();
+	move();
+}
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	solve();
+	return 0;
+}
