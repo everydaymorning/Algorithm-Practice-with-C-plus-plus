cd121ba5d7a03e0573ad29088f1b80d555823c61
everydaymorning <zmflrqll@naver.com>
2020-05-31 20:36:06 +0900

    20200531 sorting implementation

--- TestProject/TestProject/quick.cpp
+++ TestProject/TestProject/quick.cpp
@@ -1,15 +1,58 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int data[] = { 1, 10, 5, 8, 7, 6, 4, 3, 2, 9 };
 int number = 10;
-
+int arr2[10];
 void quicksort(int* data, int start, int end);
-void show();
 
+void merge(int* arr, int low, int high) {
+	int mid = (low + high) / 2;
+	int i = low;
+	int j = mid + 1;
+	int k = low;
+
+	while (i <= mid && j <= high) {
+		if (arr[i] <= arr[j]) {
+			arr2[k++] = arr[i++];
+		}
+		else {
+			arr2[k++] = arr[j++];
+		}
+	}
+
+	int tmp = i > mid ? j : i;
+
+	if (i > mid) {
+		for (int a = j; a <= high; a++) {
+			arr2[k++] = arr[a];
+		}
+	}
+	else {
+		for (int a = i; a <= mid; a++) {
+			arr2[k++] = arr[a];
+		}
+	}
+
+	for (int a = low; a <= high; a++) {
+		arr[a] = arr2[a];
+	}
+}
+void partition(int* arr, int low, int high) {
+	if (low >= high) return;
+
+	int mid = (low + high) / 2;
+
+	partition(arr, low, mid);
+	partition(arr, mid + 1, high);
+	merge(arr, low, high);
+}
 int main() {
-	quicksort(data, 0, number - 1);
-	show();
-	return 0;
+	int arr[10] = { 1, 10, 5, 8, 7, 6, 4, 3, 2, 9 };
+
+	partition(arr, 0, 9);
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << " ";
+	}
 }
 
 void quicksort(int* data, int start, int end) {
@@ -44,10 +87,4 @@ void quicksort(int* data, int start, int end) {
 
 	quicksort(data, start, j - 1);
 	quicksort(data, j + 1, end);
-}
-
-void show() {
-	for (int i = 0; i < number; i++) {
-		printf("%d ", data[i]);
-	}
 }
